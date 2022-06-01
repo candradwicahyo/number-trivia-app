94627cc3785503fe2cfcc742be1f764865bc5121
candradwicahyo <candradwicahyo18@gmail.com>
2022-06-01 12:46:41 +0700

    first commit

---
+++ Readme.md
@@ -0,0 +1,30 @@
+## Trivia App
+
+sebuah program dengan nama number trivia app yang dibuat dengan framework bootstrap dan bahasa pemrograman javascript. masukkan sebuah angka antara 0 sampai 300, maka akan muncul beberapa fakta mengenai angka tersebut.
+
+### dibuat dengan berikut 
+
+* HTML
+* CSS
+  * [Bootstrap](https://getbootstrap.com)
+* javascript
+
+### lainnya
+
+* [API](http://numbersapi.com)
+* [SweetAlert2](https://sweetalert2.github.io)
+* handphone
+* code Editor (Acode)
+* edit gambar (Pixellab)
+
+### preview
+
+![result](https://github.com/candradwicahyo/number-trivia-app/blob/master/preview.jpg)
+
+terinspirasi dari channel [Coding Artist](https://m.youtube.com/watch?v=RoGRFGbeIGk&t=1147s)
+
+
+[tekan link ini untuk mencoba program ini](https://candradwicahyo.github.io/number-trivia-app)
+
+
+> dibuat oleh **candra dwi cahyo**
---
+++ script.js
@@ -0,0 +1,56 @@
+window.onload = () => {
+  
+  const number = document.querySelector('.number');
+  const text = document.querySelector('.text');
+  
+  let url = 'http://numbersapi.com/';
+  
+  async function getRandomFact() {
+    const limit = 300;
+    const getRandomNumber = Math.floor((Math.random() * limit) + 1);
+    const data = await getData(getRandomNumber);
+    putValue(getRandomNumber, data);
+  }
+  
+  getRandomFact();
+  
+  function getData(param) {
+    return fetch(url + param)
+      .then(response => response.text())
+      .then(response => response)
+  }
+  
+  function putValue(randomNumber, data) {
+    number.textContent = randomNumber;
+    text.textContent = data;
+  }
+  
+  const getRandomFactButton = document.querySelector('.random');
+  getRandomFactButton.addEventListener('click', getRandomFact);
+  
+  const input = document.querySelector('.input');
+  const getFactButton = document.querySelector('.submit');
+  getFactButton.addEventListener('click', async () => {
+    if (validate(input.value) == true) {
+      const value = input.value;
+      const data = await getData(value);
+      putValue(value, data);
+    }
+  });
+  
+  function validate(number) {
+    if (!number) return sweetalert('error', 'Error', 'isi input terlebih dshulu!');
+    if (number < 0 || number > 300) return sweetalert('error', 'Error', 'isi input antara 0 sampai 300');
+    if (number.match(/[a-zA-Z]/gmi)) return sweetalert('error', 'Error', 'input hanya boleh berisikan angka saja!');
+    return true;
+  }
+  
+  function sweetalert(icon, title, text) {
+    return swal.fire ({
+      icon: icon,
+      title: title,
+      text: text
+    });
+  }
+  
+}
